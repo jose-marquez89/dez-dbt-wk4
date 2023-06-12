6f2cf74aca2b064c765d15a1afc1f23beb3ca75d
Jose Marquez <jose_marquez89@icloud.com>
2023-06-12 11:18:27 +0000

    adds code for completed models

--- models/core/fact_trips.sql
+++ models/core/fact_trips.sql
@@ -23,9 +23,36 @@ dim_zones as (
     where borough <> 'Unknown'
 )
 
-select *
+select
+    t.trip_id,
+    t.vendorid,
+    t.service_type,
+    t.ratecodeid,
+    t.pu_loc_id,
+    pz.borough as pu_borough,
+    pz.zone as pu_zone,
+    t.do_loc_id,
+    dz.borough as do_borough,
+    dz.zone as do_zone,
+    t.pickup_datetime,
+    t.dropoff_datetime,
+    t.store_and_fwd_flag,
+    t.passenger_count,
+    t.trip_distance,
+    t.trip_type,
+    t.fare_amount,
+    t.extra,
+    t.mta_tax,
+    t.tip_amount,
+    t.tolls_amount,
+    t.ehail_fee,
+    t.improvement_surcharge,
+    t.total_amount,
+    t.payment_type,
+    t.pay_type_desc,
+    t.congestion_surcharge
 from trips_unioned t
-join dim_zones pz
+inner join dim_zones as pz
     on t.pu_loc_id = pz.locationid
-join dim_zones dz
+inner join dim_zones as dz
     on t.do_loc_id = dz.locationid
